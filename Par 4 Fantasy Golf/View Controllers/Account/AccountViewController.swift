398896648fdda36a6a0a04e7fb75d59c6b6c0f39
Brian Aguirre <bpaguirre.93@gmail.com>
2023-05-03 16:17:49 -0400

    require the user to confirm signing out

--- Par 4 Fantasy Golf/View Controllers/Account/AccountViewController.swift
+++ Par 4 Fantasy Golf/View Controllers/Account/AccountViewController.swift
@@ -40,16 +40,41 @@ class AccountViewController: UIViewController {
     // Attempt to sign the user out
     @IBAction func signOutPressed() {
         
-        // If there is an error, present it to the user; otherwise, transition to the sign in view controller
-        do {
-            try Auth.auth().signOut()
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
-            return
+        // Display a confirmation alert
+        let confirmAlert = UIAlertController(title: "Are you sure?", message: "You won't be able to access any of your data until you sign in again.", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let signOutAction = UIAlertAction(title: "Sign Out", style: .destructive) { action in
+            
+            // Dismiss the current alert
+            confirmAlert.dismiss(animated: true)
+            
+            // If there is an error, present it to the user; otherwise, transition to the sign in view controller
+            do {
+                try Auth.auth().signOut()
+            } catch let error as NSError {
+                
+                guard let errorCode = AuthErrorCode.Code(rawValue: error.code) else {
+                    self.displayAlert(title: "Sign Out Error", message: "Something went wrong, but we're not exactly sure why. If you continue to see this message, reach out to the developer for assistance.")
+                    return
+                }
+                
+                switch errorCode {
+                case .networkError:
+                    self.displayAlert(title: "Sign Out Error", message: "Looks like there was a network issue. Your connection could be slow, it may have been interrupted, or the server could be temporarily unreachable.")
+                default:
+                    self.displayAlert(title: "Sign Out Error", message: "Something went wrong, but we're not exactly sure why. If you continue to see this message, reach out to the developer for assistance.")
+                }
+                
+                return
+            }
+            
+            self.transitionToAuthenticationViewController()
         }
         
-        print("Sign out successful!")
-        transitionToAuthenticationViewController()
+        confirmAlert.addAction(signOutAction)
+        confirmAlert.addAction(cancelAction)
+        
+        present(confirmAlert, animated: true)
     }
     
     // MARK: - Navigation
@@ -57,11 +82,13 @@ class AccountViewController: UIViewController {
     // Transition to the main tab bar controller
     // Is there a better way to do this? Seems a little wonky
     func transitionToAuthenticationViewController() {
+        guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let authenticationRootViewController = storyboard.instantiateViewController(withIdentifier: "AuthenticationRoot")
         
         // Set a new root view controller
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.setRootViewController(to: authenticationRootViewController)
+        sceneDelegate.setRootViewController(to: authenticationRootViewController)
     }
     
 
